81674d7bf61d848fa5e0db59b589f559a73851d0
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-06-06 13:59:23 +0530

    feat:extracting palettes from image

--- components/image/ImageCanvas.jsx
+++ components/image/ImageCanvas.jsx
@@ -1,6 +1,8 @@
-import { Box, Flex } from "@chakra-ui/react";
-import { useRef, useEffect } from "react";
+import { Box, Flex, Button } from "@chakra-ui/react";
+import { useRef, useEffect, useState } from "react";
 import Draggable from "react-draggable";
+import Palette from "../palette/Palette";
+
 const ImageCanvas = ({ url }) => {
   let canvasRef = useRef();
   let parent = useRef();
@@ -9,49 +11,50 @@ const ImageCanvas = ({ url }) => {
   let picker3 = useRef();
   let picker4 = useRef();
   let picker5 = useRef();
-
-
+  const [palettes,setPalettes] = useState([]);
   useEffect(() => {
+    setPalettes([]);
     const canvas = canvasRef.current;
     const ctx = canvas.getContext("2d");
     let img = new Image();
+    img.src = `${url}`;
+    img.crossOrigin = "Anonymous";
     const drawActual = () => {
       canvas.width = img.naturalWidth;
       canvas.height = img.naturalHeight;
       ctx.drawImage(img, 0, 0, img.width, img.height);
     };
     img.onload = drawActual;
-    img.src = `${url}`;
-    img.crossOrigin = "Anonymous";
-    
-    let initialXYpositions = pickersXYpos();
-    let initialPalettes =pickerPalettes(initialXYpositions);
-    console.log(initialPalettes);
-    
+  },[url]);
 
-  }, [url]);
-  const pickerPalettes=(positions)=>{
-      let result=[];
-      for(let i of positions){
-          let x = i.x;
-          let y=i.y;
-          let rgb = getRgb(x,y);
-          result.push(rgb);
-      }
-      return result;
-  }
-  const pickersXYpos=()=>{
-    let result=[];
-    let parentRect = parent.current.getBoundingClientRect();
-    let pickers =[picker1.current,picker2.current,picker3.current,picker4.current,picker5.current];
-    for(let i of pickers){
-        let childRect = i.getBoundingClientRect();
-        let x = childRect.left - parentRect.left;
-        let y = childRect.top - parentRect.top;
-        result.push({x,y});
+  const pickerPalettes = (positions) => {
+    let result = [];
+    for (let i of positions) {
+      let x = i.x;
+      let y = i.y;
+      let rgb = getRgb(x, y);
+      result.push(rgb);
     }
     return result;
-  }
+  };
+  const pickersXYpos = () => {
+    let result = [];
+    let parentRect = parent.current.getBoundingClientRect();
+    let pickers = [
+      picker1.current,
+      picker2.current,
+      picker3.current,
+      picker4.current,
+      picker5.current,
+    ];
+    for (let i of pickers) {
+      let childRect = i.getBoundingClientRect();
+      let x = childRect.left - parentRect.left;
+      let y = childRect.top - parentRect.top;
+      result.push({ x, y });
+    }
+    return result;
+  };
 
   const rgbToHex = (r, g, b) => {
     r = r.toString(16);
@@ -63,10 +66,9 @@ const ImageCanvas = ({ url }) => {
     return "#" + r + g + b;
   };
 
-  const getRgb =async  (x, y) => {
+  const getRgb = (x, y) => {
     let params = canvasRef.current.getContext("2d");
-    console.log(x,y);
-    let squareImage =await params.getImageData(x, y, 1, 1);
+    let squareImage = params.getImageData(x, y, 1, 1);
     let colorData = squareImage.data;
     let rgb = rgbToHex(colorData[0], colorData[1], colorData[2]);
     return rgb;
@@ -77,9 +79,14 @@ const ImageCanvas = ({ url }) => {
     let pickerRect = e.target.getBoundingClientRect();
     let x = pickerRect.left - parentRect.left;
     let y = pickerRect.top - parentRect.top;
-
     const rgb = getRgb(x, y);
-    console.log(rgb);
+    return rgb;
+  };
+  const generatePalette = () => {
+    let initialXYpositions = pickersXYpos();
+    let imagePalattes = pickerPalettes(initialXYpositions);
+    console.log(imagePalattes);
+    setPalettes(imagePalattes);
   };
   const throttle = (cb) => {
     let timer = false;
@@ -92,11 +99,11 @@ const ImageCanvas = ({ url }) => {
     };
   };
   return (
-    <Flex h="100%" w="100%" justifyContent="center" alignItems="center">
+    <Box>
       <Box h="80%" w="100%" position="relative" ref={parent}>
         <canvas
-          style={{ height: "100%", width: "100%" }}
           ref={canvasRef}
+          style={{ height: "100%", width: "100%" }}
         ></canvas>
         <Draggable
           bounds="parent"
@@ -184,7 +191,19 @@ const ImageCanvas = ({ url }) => {
           ></Box>
         </Draggable>
       </Box>
-    </Flex>
+      <Flex flexDirection="row" mt="1rem" gap={1}>
+        {
+          palettes.map((palette,index)=>{
+            return <Palette key={palette+index} palette={palette}/>
+          })
+        }
+      </Flex>
+      <Box mt="1rem">
+        <Button onClick={generatePalette} colorScheme="messenger">
+          Generate Palatte
+        </Button>
+      </Box>
+    </Box>
   );
 };
 
---
+++ components/palette/Palette.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+
+
+const Palette = ({palette}) => {
+    return (
+        <Box h="2rem" bgColor={palette} flexGrow={1}>
+            
+        </Box>
+    )
+}
+
+export default Palette
