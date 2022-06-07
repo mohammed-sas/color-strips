be29281195d761e0b0f9a3bc65b492ae9650712a
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-06-07 14:30:01 +0530

    feat:added favicon and document title

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { PaletteProvider } from "../context/palette-context";
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
       <PaletteProvider>
+      <Head>
+        <title>Color Strips</title>
+      </Head>
         <Component {...pageProps} />
       </PaletteProvider>
     </ChakraProvider>
